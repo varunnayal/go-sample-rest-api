4aa3274be342efc7dc46d1f4a128aa5a0bb66b36
Varun <varunnayal@gmail.com>
2022-01-09 03:26:33 +0530

    feat: add new recipe in array

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+var recipes []Recipe
 
 type Recipe struct {
 	ID           string    `json:"id"`
@@ -13,10 +19,34 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+func _NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe;
+	if err := c.BindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	gin.DisableConsoleColor()
+	// initApp()
 
 	// router with logger and crash free middleware
 	router := gin.Default()
+	router.GET("/recipes", func (c *gin.Context) {
+		c.JSON(200, gin.H{"a": "b"})
+	})
+	router.POST("/recipes", _NewRecipeHandler)
 	router.Run(":8080")
 }
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
