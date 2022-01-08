955eff534fbd9973cbaf1574c6b0845cb1cc9e79
Varun <varunnayal@gmail.com>
2022-01-09 03:01:13 +0530

    feat: recipe model

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
 func main() {
 	gin.DisableConsoleColor()
 
